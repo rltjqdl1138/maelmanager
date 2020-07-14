50e0a21a32196960e63524c16da54f2755f9ad8a
rltjqdl1138 <rltjqdl1138@naver.com>
2020-07-15 02:35:02 +0900

    Userlist module appneded

--- src/containers/UserList.js
+++ src/containers/UserList.js
@@ -27,9 +27,16 @@ class UserList extends Component{
             searchValue:0
         }
     }
+    componentDidMount(){
+        this.getUserList()
+    }
+    getUserList = async( )=>{
+        const response = await axios.get('/api/account')
+        this.handleChange('list', response.data.data)
+    }
     handleChange = (field, value) => this.setState({ [field]: value })
     render(){
-        const {searchValue} = this.state
+        const {list, searchValue} = this.state
         return (
             <div className="MainContainer">
                 <div className="MainTitle">
@@ -47,9 +54,111 @@ class UserList extends Component{
                     searchTitle="검색어"
                     dateTitle="가입일"
                 />
+                <UserInfo info={list}
+                    handleChange={(index)=>console.log(index)}
+                />
             </div>
         )
     }
+}
+
+class UserInfo extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            page:1,
+            limit:10
+        }
+    }
+    handleChange = (field, value) =>
+        this.setState({ [field]: value })
+    
+    getPagelist = (page, total, limit)=>{
+        const totalPage = Math.ceil(total / limit)
+        const start = Math.max(page-5,1)
+        const end = Math.min(totalPage, start+9)
+        let result = []
+        for(let i = start; i <= end; i++){
+            result = [...result, i]
+        }
+        const component = result.map(item=>{
+            return (
+                <td key={item} >
+                    <button className={item===page ? "SelectedPage":"UnselectedPage"}
+                        onClick={()=>this.handleChange('page',item)}>{item}</button>
+                </td>
+            ) })
+        return (
+            <table>
+                <tbody>
+                    <tr>{component}</tr>
+                </tbody>
+            </table>
+        )
+    }
+    render(){
+        const {info} = this.props
+        const {page, limit } = this.state
+        const total = info.length
+        const start = (page-1)*limit
+        const pageList = this.getPagelist(page, total, limit)
+        const soundinfoList = info.slice( start, start + limit).map((item, index)=>{
+            const date = item.createdTime ? new Date(item.createdTime) : null
+            return (
+                <tr key={index} >
+                    <td>
+                        <input type="checkbox"
+                            onChange={()=>this.props.handleChange(item.id)}
+                        />
+                    </td>
+                    <td>{item.platform}</td>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.birthday}</td>
+                    <td>{item.mobile}</td>
+                    <td>{item.stateID}</td>
+                    <td>0000-00-00</td>
+                    <td>{date?date.toLocaleDateString():'-'}</td>
+                    <td>{item.stateID}</td>
+                </tr>
+            )
+        })
+        return(
+            <div className="MainContentContainer">
+                <div className="SoundInfoText">
+                    <div>전체</div>
+                    <div style={{color:'red'}}>{total}</div>
+                    <div>개 / 검색</div>
+                    <div style={{color:'red'}}>{limit}</div>
+                    <div>개</div>
+                </div>
+                <div className="MainSubtitle">
+                    사운드정보
+                </div>
+                <table className="SoundInfoTable">
+                    <thead className="TableTitle">
+                        <tr >
+                            <th>선택</th>
+                            <th>구분</th>
+                            <th>아이디</th>
+                            <th>이름</th>
+                            <th>생년월일</th>
+                            <th>휴대전화</th>
+                            <th>플랜</th>
+                            <th>결제기준일</th>
+                            <th>회원가입일</th>
+                            <th>휴면여부</th>
+                        </tr>
+                    </thead>
+                    <tbody className="TableItem">
+                        {soundinfoList}
+                    </tbody>
+                </table>
+                {pageList}
+            </div>
+        )
+    }
+    
 }
 
 
